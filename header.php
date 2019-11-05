988cf63ce9b331147a2b4482c6563d7ba087117d
oliv1063 <57345179+oliv1063@users.noreply.github.com>
2019-11-05 09:49:03 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="Oliver B. Joergensen">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body>
+<div class="container-fluid">
+ <div class="container">
+  <div class="row">
+    <div class="col">
+      1 of 5
+    </div>
+    <div class="col">
+      1 of 5
+    </div>
+    <div class="col">
+      1 of 5
+    </div>
+      <div class="col">
+      1 of 5
+    </div>
+      <div class="col">
+      1 of 5
+    </div>
+  </div>
+</div>
+</div>
+    
+    
+    </body>
