5f9e2cdacebbb7e72501465369523f3543de191b
oliv1063 <57345179+oliv1063@users.noreply.github.com>
2019-11-05 11:42:52 +0100

    head

--- includes/header.php
+++ includes/header.php
@@ -5,25 +5,33 @@
     <meta charset="utf-8">
     <meta name="author" content="Oliver B. Joergensen">
     <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+    <!-- Kilde til icon biblotek fra bootstrap 4 -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <!-- Link til jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- Link til Javascript -->
+    <script src="../js/bootstrap.js"></script>
 </head>
 
 <body>
-    <div class="container-fluid fixed-bottom bg-primary">
+    <div class="container-fluid fixed-bottom d-lg-none">
         <div class="row">
             <div class="col">
-                1 of 5
+                <!-- Style skal flyttes til CSS fil-->
+                <i class="fas fa-home" style="font-size:3em; color:black;"></i>
+                <p>HOME</p>
             </div>
             <div class="col">
-                1 of 5
+               <i class="fas fa-search" style="font-size:3em; color:black;"></i>
             </div>
             <div class="col">
-                1 of 5
+                <i class="fas fa-plus-circle" style="font-size:3em; color:black;"></i>
             </div>
             <div class="col">
-                1 of 5
+                <i class="far fa-comment-dots" style="font-size:3em; color:black;"></i>
             </div>
             <div class="col">
-                1 of 5
+               <i class="fas fa-user" style="font-size:3em; color:black;"></i>
             </div>
         </div>
     </div>
