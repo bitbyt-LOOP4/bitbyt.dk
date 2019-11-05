0cc23e184b43db4e48e4bd2dfaf9d486f20e8186
oliv1063 <57345179+oliv1063@users.noreply.github.com>
2019-11-05 10:15:16 +0100

    header

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="Oliver B. Joergensen">
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+</head>
+
+<body>
+    <div class="container-fluid fixed-bottom bg-primary">
+        <div class="row">
+            <div class="col">
+                1 of 5
+            </div>
+            <div class="col">
+                1 of 5
+            </div>
+            <div class="col">
+                1 of 5
+            </div>
+            <div class="col">
+                1 of 5
+            </div>
+            <div class="col">
+                1 of 5
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
