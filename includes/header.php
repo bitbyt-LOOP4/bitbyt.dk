c3295ba2be8c9fc454e4bbab58ab72fd5063570d
jesperviborg <57345095+jesperviborg@users.noreply.github.com>
2019-11-06 09:36:35 +0100

    update

--- includes/header.php
+++ includes/header.php
@@ -1,30 +1,9 @@
-<?php
-    $menu = '
-		<ul class="navbar-nav ml-auto">
-						<li class="nav-item">
-							<a class="nav-link js-scroll-trigger" href="faq.php">
-							Hvordan virker det?</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link js-scroll-trigger" href="register.php">
-							Registrer</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link js-scroll-trigger" href="index.php">
-							Login</a>
-						</li>
-						</ul>
-	
-	';
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
-    <meta name="author" content="Oliver B. Joergensen">
+    <meta name="author" content="bitbyt.dk">
     <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
     <!-- Kilde til icon biblotek fra bootstrap 4 -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
@@ -36,7 +15,28 @@
 </head>
 
 <body>
+    <!-- Det her er til top navigationen på computeren -------------------------------------------------->
+    <nav class="navbar navbar-expand-sm bg-bitbyt">
+        <!-- Brand/logo -->
+        <a class="navbar-brand" href="#">
+            <img src="../images/logo_transparent1.png" alt="logo" class="logo">
+        </a>
 
+        <!-- Links -->
+        <ul class="nav navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Sådan virker det</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Log ind</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="btn btn-info" role="button">Registrer</a>
+            </li>
+        </ul>
+    </nav>
+
+    <!-- Det her er til navigation på tablet og mobil -------------------------------------------------->
     <div class="container-fluid fixed-bottom d-lg-none container_head">
         <div class="row">
             <div class="col-2 bottum_links text-center">
@@ -44,7 +44,7 @@
                 <!-- Style skal flyttes til CSS fil-->
                 <a class="icon_text" href="*">
                     <i class="fas fa-home bottum_icons"></i>
-                Hjem
+                    Hjem
                 </a>
 
             </div>
