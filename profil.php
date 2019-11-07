67f02bec1dbbbd45b922ee0472927a426a157537
oliv1063 <57345179+oliv1063@users.noreply.github.com>
2019-11-07 14:34:36 +0100

    Update profil.php

--- profil.php
+++ profil.php
@@ -1,20 +1,7 @@
-<!-- DETTE SKAL INCLUDES MED PHP PÅ ET TIDSPUNKT, OG DERFOR SKAL DET SLETTES INDEN -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="author" content="bitbyt.dk">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-    <!-- Kilde til icon biblotek fra bootstrap 4 -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-    <!-- Link til jquery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <!-- Link til Javascript -->
-    <script src="js/bootstrap.js"></script>
-    <link rel="stylesheet" href="styles/style.css">
-</head>
-    <!-- DETTE SKAL INCLUDES MED PHP PÅ ET TIDSPUNKT, OG DERFOR SKAL DET SLETTES INDEN -->
-<body>
+<?php 
+$page='Min profil';
+require_once('includes/header.php');
+?>
     
     
 
@@ -73,6 +60,10 @@
     </div>
     </div>
     
+
+<?php 
+require_once('includes/footer.php');
+?>
     
     
     
