a88ab9568666f8cd491856da08d9da43b5c5b9b9
oliv1063 <57345179+oliv1063@users.noreply.github.com>
2019-11-07 12:20:46 +0100

    profil

--- profil.php
+++ profil.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="author" content="bitbyt.dk">
-    <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
     <!-- Kilde til icon biblotek fra bootstrap 4 -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     <!-- Link til jquery -->
@@ -19,27 +19,48 @@
     
 
 <!-- Link til Avatar https://publicdomainvectors.org/en/free-clipart/User-3-avatar-vector-clip-art/13634.html -->
-      <div class="row">
-    <div class="col-12 test1">
-        <span>hej med jer</span>
+    
+<div class="container-fluid">
+  <div class="row">
+      <div class="col-2 profil_container"></div>
+    <div class="col-8 profil_container">
+        <div class="text-center">
+        <img class="avatar" src="images/avatar.PNG">
+    <!-- Brugernavn hentes via PHP -->
+        <p class="profil_brugernavn">"brugernavn"</p>
+        </div>
+      </div>
+      <div class="col-2 profil_container">
+          <div class="list-group text-right">
+  
+  <a href="#" class="list-group-item list-group-item-action">Indstillinger</a>
+  <a href="#" class="list-group-item list-group-item-action">Mine Hubs</a>
+  <a href="#" class="list-group-item list-group-item-action">Historik</a>
+
+</div>
+          
+      </div>
+      
+    </div>
+    <div class="row text-center">
+        <div class="col-4">
+            <span class="profil_variabler">2</span>
+            <br>
+            <span class="under_profil">Hubs</span>
+        </div>
+        <div class="col-4">
+            <span class="profil_variabler">6</span>
+            <br>
+            <span class="under_profil">Opslag</span>
+        </div>
+        <div class="col-4">
+            <span class="profil_variabler">10</span>
+            <br>
+            <span class="under_profil">Byttehandler</span>
+        </div>
     
     </div>
-    <div class="">
-    <img src="images/avatar.PNG" class="rounded float-right" alt="Responsive image">
-          </div>
-          <div>
-    <span class="minprofil_brugernavn">"Brugernavn fra PHP"</span>
     </div>
-    <div class="col-12 test3">
-        <span>what</span>
-    
-    </div>
-  </div>
-    
-    
-    
-    
-    
     
     
     
