bc0b6d4ba370cb30e2da6bf5d72b5962d36df821
oliv1063 <57345179+oliv1063@users.noreply.github.com>
2019-11-08 13:05:44 +0100

    pray

--- index.php
+++ index.php
@@ -1,27 +1,15 @@
-<<<<<<< HEAD
-<h1>hej</h1>
 
-=======
->>>>>>> profil
 <?php 
 $page='Index';
 require_once('includes/header.php');
 ?>
 
-<<<<<<< HEAD
-=======
+
 <h1>hej</h1>
 
 
->>>>>>> profil
 
 
 <?php 
 require_once('includes/footer.php');
 ?>
-<<<<<<< HEAD
-    
-    
-=======
-   
->>>>>>> profil
