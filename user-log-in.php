760f9ca061a6b06c3c3bb113b3800e1cd2f7189d
oliv1063 <57345179+oliv1063@users.noreply.github.com>
2019-11-17 11:06:58 +0100

    h

--- user-log-in.php
+++ user-log-in.php
@@ -1,6 +1,39 @@
 <?php
 $page = 'Log ind';
+require_once('conn.php');
 require_once('includes/header.php');
+if(isset($_SESSION['user_id'])) {
+    header('location:loggedin_front.php');
+}
+else{
+    if(isset($_POST['username']) && isset($_POST['password'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $query = "SELECT * FROM kid WHERE username = '$username'";
+        $result = mysqli_query($con, $query);
+        if (!$result) die(mysqli_error);
+        else {
+            $rows = mysqli_num_rows($result);
+            if ($rows == 0) {
+                echo "Denne bruger eksiterer ikke";
+            }
+            else {
+                if ($rows > 0) {
+                    while($row = mysqli_fetch_assoc($result)){
+                        $username = $row['username'];
+                        $database_password = $row['password'];
+                        $kid_id = $row['kid_id'];
+                        $token = password_verify('$password', '$database_password');
+                        if ($token == $password) {
+                            $_SESSION['kid_id'] = $kid_id;
+                            echo "Velkommen " . $username . "!";
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 ?>
 
 
@@ -8,8 +41,8 @@ require_once('includes/header.php');
     <form class="form-signin" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
         <br><br><br>
         <h1 class="h3 mb-3 font-weight-normal">Log ind</h1>
-        <label for="inputEmail" class="sr-only">E-mail</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus value="<?php echo $email; ?>">
+        <label for="inputEmail" class="sr-only">Username</label>
+        <input type="username" id="inputUsername" class="form-control" placeholder="Username" required autofocus value="<?php echo $username; ?>">
         <label for="inputPassword" class="sr-only">Password</label>
         <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
         <div class="checkbox mb-3">
