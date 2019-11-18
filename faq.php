145f68a3739a957afcbe74e25c698d1c1329751d
oliv1063 <57345179+oliv1063@users.noreply.github.com>
2019-11-18 14:06:27 +0100

    j

--- faq.php
+++ faq.php
@@ -3,70 +3,242 @@ $page='bitbyt.dk';
 require_once('includes/header.php');
 ?>
 
+<!-- Nr 1. -->
 <div class="jumbotron jumbotron-fluid bg-bitbyt mb-0 mt-0">
     <div class="container">
         <h1 class="display-4 mb-10 about_header">Sådan virker det!</h1>
         <hr>
         <br>
         <div class="row">
-            <div style="background-color:whitesmoke; border: solid black;" class="col-4 text-left">
-                 <h6 class="mb-2 text-center">1. Opret bruger</h6>
+            <div class="col-12 col-md-4 col-xl-4 text-center d-none d-md-block faq_box">
+            <h6 class="mb-2 mt-2 text-center">1. Opret bruger</h6>
                 <p> Opret en bruger ved hjælp af "Registrer" knappen. </p>
-        </div>
-        <div class="col-4 text-center">
-        <img class="icon_index" src="images/opret-bruger.svg">
-        </div>
-        <div class="col-4 text-center">
-        </div>
-    </div>
-</div>
-
-
-    <div class="container">
-        <div class="row">
-        <div class="col-4"></div>
-        <div class="col-4 text-center mt-5 mb-5">
-            <img class="arrow" src="images/arrow-down-sign-to-navigate.svg">
             </div>
-        <div class="col-4"></div>
+            <div class="col-12 col-md-4 col-xl-4 text-center">
+              <img class="icon_index mb-3" src="images/opret-bruger.svg">
+            </div>
+            <div class="col-12 col-md-4 col-xl-4 text-center d-md-none faq_box">
+                <h6 class="mb-2 text-center">1. Opret bruger</h6>
+                <p> Opret en bruger ved hjælp af "Registrer" knappen. </p>
+            </div>
+        </div>
+    </div> 
 
-        </div>
-        </div>
-    
-    
-    
+
     <div class="container">
         <div class="row">
             <div class="col-4"></div>
-        <div class="col-4 text-center">
-        <img class="icon_index" src="images/opret-bruger.svg">
+            <div class="col-4 text-center mt-5 mb-5">
+                <img class="arrow" src="images/arrow-down-sign-to-navigate.svg">
+            </div>
+            <div class="col-4"></div>
+
         </div>
-        <div style="background-color:whitesmoke; border: solid black;" class="col-4 text-center">
-                 <h6 class="mb-2">1. Opret bruger</h6>
-                <p> Opret en bruger ved hjælp af "Registrer" knappen. </p>
+    </div> 
+
+    
+    
+    
+    
+    <!-- Nr 2. -->
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-4 col-xl-4 text-center">
+            </div>
+            <div class="col-12 col-md-4 col-xl-4 text-center">
+              <img class="icon_index mb-3" src="images/school-icon.svg">
+            </div>
+            <div class="col-12 col-md-4 col-xl-4 text-center faq_box">
+                <h6 class="mb-2 mt-2 text-center">2. Tilslut Hubs</h6>
+                <p> Skriv noget om hubs </p>
+            </div>
         </div>
     </div>
-</div>
 
 
     <div class="container">
         <div class="row">
-        <div class="col-4"></div>
-        <div class="col-4 text-center mt-5">
-            <img class="arrow" src="images/arrow-down-sign-to-navigate.svg">
+            <div class="col-4"></div>
+            <div class="col-4 text-center mt-5 mb-5">
+                <img class="arrow" src="images/arrow-down-sign-to-navigate.svg">
             </div>
-        <div class="col-4"></div>
+            <div class="col-4"></div>
 
         </div>
+    </div>
+    
+    
+    <!-- Nr 3. -->
+     <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-4 col-xl-4 text-center d-none d-md-block faq_box">
+            <h6 class="mb-2 mt-2 text-center">3. Upload</h6>
+                <p> Billed Upload dimmer </p>
+            </div>
+            <div class="col-12 col-md-4 col-xl-4 text-center">
+              <img class="icon_index mb-3" src="images/photo-camera.svg">
+            </div>
+            <div class="col-12 col-md-4 col-xl-4 text-center d-md-none faq_box">
+                <h6 class="mb-2 text-center">3. Upload</h6>
+                <p> Billed Upload dimmer </p>
+            </div>
         </div>
+    </div>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-4"></div>
+            <div class="col-4 text-center mt-5 mb-5">
+                <img class="arrow" src="images/arrow-down-sign-to-navigate.svg">
+            </div>
+            <div class="col-4"></div>
+
+        </div>
+    </div>
+    
+    
+    
+    <!-- Nr 4. -->
+    
+     <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-4 col-xl-4 text-center">
+            </div>
+            <div class="col-12 col-md-4 col-xl-4 text-center">
+              <img class="icon_index mb-3" src="images/toys-icon.svg">
+            </div>
+            <div class="col-12 col-md-4 col-xl-4 text-center faq_box">
+                <h6 class="mb-2 mt-2 text-center">4. Se opslag</h6>
+                <p> Noget med at finde det man gerne vil have </p>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-4"></div>
+            <div class="col-4 text-center mt-5 mb-5">
+                <img class="arrow" src="images/arrow-down-sign-to-navigate.svg">
+            </div>
+            <div class="col-4"></div>
+
+        </div>
+    </div>
+    
+    <!-- Nr 5. -->
+    
+      <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-4 col-xl-4 text-center d-none d-md-block faq_box">
+            <h6 class="mb-2 mt-2 text-center">5. Anmod om byttehandel</h6>
+                <p> Tekst tekst tekst </p>
+            </div>
+            <div class="col-12 col-md-4 col-xl-4 text-center">
+              <img class="icon_index mb-3" src="images/chat-icon.svg">
+            </div>
+            <div class="col-12 col-md-4 col-xl-4 text-center d-md-none faq_box">
+                <h6 class="mb-2 text-center">5. Anmod om byttehandel</h6>
+                <p> Tekst tekst tekst </p>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-4"></div>
+            <div class="col-4 text-center mt-5 mb-5">
+                <img class="arrow" src="images/arrow-down-sign-to-navigate.svg">
+            </div>
+            <div class="col-4"></div>
+
+        </div>
+    </div>
+    
+    
+    <!-- Nr 6. -->
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-4 col-xl-4 text-center">
+            </div>
+            <div class="col-12 col-md-4 col-xl-4 text-center">
+              <img class="icon_index mb-3" src="images/check-icon.svg">
+            </div>
+            <div class="col-12 col-md-4 col-xl-4 text-center faq_box">
+                <h6 class="mb-2 mt-2 text-center">6. Accepter byttehandel</h6>
+                <p> Accepter bytteanmodninger </p>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-4"></div>
+            <div class="col-4 text-center mt-5 mb-5">
+                <img class="arrow" src="images/arrow-down-sign-to-navigate.svg">
+            </div>
+            <div class="col-4"></div>
+
+        </div>
+    </div>
+    
+    
+
+    <!-- Nr 7. -->
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-4 col-xl-4 text-center d-none d-md-block faq_box">
+            <h6 class="mb-2 text-center">7. Forældre accept</h6>
+                <p> Tekst tekst tekst </p>
+            </div>
+            <div class="col-12 col-md-4 col-xl-4 text-center">
+              <img class="icon_index mb-3" src="images/for%C3%A6ldre-icon.svg">
+            </div>
+            <div class="col-12 col-md-4 col-xl-4 text-center d-md-none faq_box">
+                <h6 class="mb-2 mt-2 text-center">7. Forældre accept</h6>
+                <p> Tekst tekst tekst </p>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-4"></div>
+            <div class="col-4 text-center mt-5 mb-5">
+                <img class="arrow" src="images/arrow-down-sign-to-navigate.svg">
+            </div>
+            <div class="col-4"></div>
+
+        </div>
+    </div>
+    
+    <!-- Nr 8. -->
+     <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-4 col-xl-4 text-center">
+            </div>
+            <div class="col-12 col-md-4 col-xl-4 text-center">
+              <img class="icon_index mb-3" src="images/chatbot-icon.svg">
+            </div>
+            <div class="col-12 col-md-4 col-xl-4 text-center faq_box">
+                <h6 class="mb-2 mt-2 text-center">8. Byt</h6>
+                <p> Aftal mødetidspunkt og byt ved jeres fælles hub </p>
+            </div>
+        </div>
+    </div>
     
     
     
     
     
-    
-    
-    
+
+
+
 </div>
 
 
