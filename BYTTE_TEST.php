e08e16cf7f8a7804f25f465ec252b800d19fcd5a
jesperviborg <57345095+jesperviborg@users.noreply.github.com>
2019-11-19 10:25:43 +0100

    sd

---
+++ BYTTE_TEST.php
@@ -0,0 +1,123 @@
+<?php
+$page = 'bitbyt';
+require_once('includes/header.php');
+if (isset($_SESSION['user_id'])) {
+$user_id = $_SESSION['user_id'];
+?>
+
+<!-- container der indeholder artikler -->
+<div class="container">
+    <div class="row">
+        <!-- LOOP der genere artikler -->
+        <?php
+         
+            
+            
+              $query = "SELECT * FROM `product` ORDER BY RAND() LIMIT 30";
+	$result = mysqli_query($con, $query);
+	if (!$result) die(mysqli_error($con));
+	else ($rows = mysqli_num_rows($result));
+
+            
+            
+            
+            
+           	 if ($rows > 0) { 
+		while($row = mysqli_fetch_array($result)) {
+			$product_name = $row['product_name'];
+			$description = $row['description'];
+			$image_link = $row['image_link'];
+			$price = $row['price'];
+            $ID = $row['product_id'];
+
+            echo '<div class="col-md-4 col-lg-3">';
+            echo '<div class="card mb-4 shadow-sm">';
+            
+            echo "<h4> $product_name </h4>";
+
+            echo '<img src="';
+            echo "$image_link";
+            echo ' "class="bd-placeholder-img card-img-top" width="100%" height="225" alt="test">';
+            echo '<div class="card-body">';
+            echo '<p class="card-text">';
+            echo $description . " " . $ID;  
+            echo '</p>';
+            echo '<div class="d-flex justify-content-between align-items-center">';
+            echo '<div class="btn-group">';
+            echo '<button type="button" class="btn btn-sm btn-outline-secondary view_data" data-toggle="modal" data-target="#myModal" id="';
+            echo $row["product_id"];
+            echo '">Se vare</button>';
+            echo '<input type="button" name="view" value="view" id="11" class="btn btn-info btn-xs view_data" />';
+            echo '</div>';
+            echo '<small class="text-muted">AUH-HUB</small>';
+            
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+            
+        }
+    } 
+
+    ?>
+
+
+
+    </div>
+    <button type="submit" class=" align-self-end btn btn-primary btn-block" onClick="window.location.reload();">Indlæs flere..</button>
+</div>
+
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title text-left">Blandet LEGO</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body">
+                <img src="./images/lego.jpg" class="bd-placeholder-img card-img-top" width="100%" height="225" alt="Lego">
+                <p>#LEGO #Blandet #Farver #Kreativ</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default btn-block">Anmod om byttehandel</button>
+            </div>
+        </div>
+
+
+        <!-- Modal content-->
+        <div class="modal-content" id="dataModal">
+            <div class="modal-header">
+                <h4 class="modal-title text-left">Blandet LEGO</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body" id="modalMain">
+                <img src="./images/lego.jpg" class="bd-placeholder-img card-img-top" width="100%" height="225" alt="Lego">
+                <p>#LEGO #Blandet #Farver #Kreativ</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default btn-block">Anmod om byttehandel</button>
+            </div>
+        </div>
+    </div>
+
+    <?php
+require_once('includes/footer.php');
+die();
+}
+/* Hvis ikke brugeren er logget ind vil siden ikke være tilgængelig */
+elseif (!isset($_SESSION['user_id'])) {
+	?>
+    <div class="container pt-5">
+        <div class=jumbotron>
+            <h1>Du har ikke adgang til denne side. Venligst log ind først.</h1>
+        </div>
+    </div>
+    <?php
+}
+require_once('includes/footer.php');
+die();
+?>
--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php 
 $page='bitbyt.dk';
 require_once('includes/header.php');
+if(isset($_SESSION['user_id'])) {
+    header('Location: feed.php');
+}
 ?>
 
 
@@ -14,24 +17,24 @@ require_once('includes/header.php');
     <div class="container">
         <h1 class="display-5 pb-3">Sådan gør du!</h1>
         <div class="row">
-        <div class="col-12 col-md-6 col-xl-3 text-center">
-            <img class="icon_index" src="images/photo-camera.svg">
-        <h6 class="mt-4 mb-4">1. Upload billeder af det du vil bytte med</h6>
-        </div>
-        <div class="col-12 col-md-6 col-xl-3 text-center">
-            <img class="icon_index" src="images/toys-icon.svg">
-        <h6 class="mt-4 mb-4">2. Find noget du gerne vil have</h6>
-        </div>
-        <div class="col-12 col-md-6 col-xl-3 text-center">
-            <img class="icon_index" src="images/chat-icon.svg">
-        <h6 class="mt-4 mb-4">3. Anmod om byttehandel</h6>
-        </div>
-        <div class="col-12 col-md-6 col-xl-3 text-center">
-            <img class="icon_index" src="images/exchange-icon.svg">
-        <h6 class="mt-4 mb-4">4. Byt!</h6>
+            <div class="col-12 col-md-6 col-xl-3 text-center">
+                <img class="icon_index" src="images/photo-camera.svg">
+                <h6 class="mt-4 mb-4">1. Upload billeder af det du vil bytte med</h6>
+            </div>
+            <div class="col-12 col-md-6 col-xl-3 text-center">
+                <img class="icon_index" src="images/toys-icon.svg">
+                <h6 class="mt-4 mb-4">2. Find noget du gerne vil have</h6>
+            </div>
+            <div class="col-12 col-md-6 col-xl-3 text-center">
+                <img class="icon_index" src="images/chat-icon.svg">
+                <h6 class="mt-4 mb-4">3. Anmod om byttehandel</h6>
+            </div>
+            <div class="col-12 col-md-6 col-xl-3 text-center">
+                <img class="icon_index" src="images/exchange-icon.svg">
+                <h6 class="mt-4 mb-4">4. Byt!</h6>
+            </div>
         </div>
     </div>
-</div>
 </div>
 <div class="jumbotron jumbotron-fluid bg-bitbyt mb-0">
     <div class="container">
